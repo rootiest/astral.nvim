d15ca0ab18ff7588d94ed75bcf782920618b2e54
rootiest <chris@rootiest.com>
2024-07-28 07:15:39 -0400

    chore: Improve Tests (#7)
    * ✅ test(astral-spec): small adjustments to tests
    
    Taking it slow this time, just made some small changes to start
    
    * ✅ test(astral-spec): try a test for restoring a theme
    
    Attempt to add another test for the restore function
    
    * 🐛 fix(astral-spec): fix new test
    
    Try to fix the new test added
    
    * 🐛 fix(astral-spec): another attempt to fix test
    
    Try to add a function to reset the state
    
    * ✅ test(astral-spec): remove test
    
    Give up for now and remove the new test
    
    * ✨ feat(astral-spec): try to add some actually useful tests
    
    Bit of a hail mary, let's see how it plays out
    
    * 🐛 fix(astral-spec): remove broken tests
    
    Well at least some of them work
    
    * ✨ feat(astral-spec): add a custom theme test
    
    I think this one will work!
    
    * 🐛 fix(astral-spec): fix custom theme test
    
    Fix the custom theme test (mid-high confidence)
    
    The previous attempts failed due to lack of available colorschemes
    
    * ✨ feat(astral-spec): new test parameters
    
    Now we are really doing tests!
    
    * 🐛 fix(astral-spec): fix tests
    
    Fix the 2 tests that failed

--- tests/astral/astral_spec.lua
+++ tests/astral/astral_spec.lua
@@ -1,12 +1,70 @@
 local astral = require("astral")
 
+local function reload()
+  for name, _ in pairs(package.loaded) do
+    if name:match("^astral") then
+      package.loaded[name] = nil
+    end
+  end
+  vim.g.COLORTHEME = nil
+end
+
 describe("setup", function()
+  before_each(function()
+    -- Clear any previous state
+    reload()
+    astral = require("astral")
+    assert.equals(nil, vim.g.COLORTHEME)
+  end)
   it("works with default", function()
-    print("hello world")
+    astral.setup()
+    print("Test successful")
   end)
 
   it("works with custom var", function()
-    astral.setup({ fallback_themes = { "catppuccin", "tokyonight", "default" } })
-    print("custom hello world")
+    astral.setup({ fallback_themes = { "evening", "default" } })
+    assert.equals(vim.g.COLORTHEME, vim.g.colors_name)
+    assert.equals("evening", vim.g.COLORTHEME)
+    print("Test successful")
+  end)
+end)
+
+describe("astral commands", function()
+  before_each(function()
+    -- Clear any previous state
+    reload()
+    astral = require("astral")
+  end)
+  it("reset", function()
+    astral.setup()
+    vim.cmd("Astral reset")
+    assert.equals("default", vim.g.COLORTHEME)
+    print("Test successful")
+  end)
+  it("reload", function()
+    astral.setup()
+    vim.cmd("colorscheme evening")
+    vim.cmd("Astral restore")
+    assert.equals(vim.g.COLORTHEME, vim.g.colors_name)
+    print("Test successful")
+  end)
+end)
+
+describe("astral themes", function()
+  before_each(function()
+    -- Clear any previous state
+    reload()
+    astral = require("astral")
+  end)
+  it("works with default", function()
+    astral.setup()
+    assert.equals(vim.g.COLORTHEME, vim.g.colors_name)
+    print("Test successful")
+  end)
+  it("works with custom", function()
+    astral.setup()
+    vim.cmd("colorscheme evening")
+    assert.equals(vim.g.COLORTHEME, vim.g.colors_name)
+    print("Test successful")
   end)
 end)

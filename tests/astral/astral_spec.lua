ddc3c4f85b7a3bb662e0dd5a92a28352e08be4b8
rootiest <chris@rootiest.com>
2024-07-28 06:36:08 -0400

    ✅ test(astral-spec): try a test for restoring a theme
    Attempt to add another test for the restore function

--- tests/astral/astral_spec.lua
+++ tests/astral/astral_spec.lua
@@ -10,4 +10,11 @@ describe("setup", function()
     astral.setup({ fallback_themes = { "catppuccin", "tokyonight", "default" } })
     print("Test successful")
   end)
+
+  it("restores colorscheme from vim.g.COLORTHEME", function()
+    vim.g.COLORTHEME = "gruvbox"
+    astral.setup()
+    astral.restore_colorscheme()
+    assert.are.equal("gruvbox", vim.g.colors_name)
+  end)
 end)

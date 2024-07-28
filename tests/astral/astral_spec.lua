391347ab2269fb43377b869f3dcae52bfac81174
rootiest <chris@rootiest.com>
2024-07-28 06:41:32 -0400

    🐛 fix(astral-spec): another attempt to fix test
    Try to add a function to reset the state

--- tests/astral/astral_spec.lua
+++ tests/astral/astral_spec.lua
@@ -1,6 +1,12 @@
 local astral = require("astral")
 
 describe("setup", function()
+  before_each(function()
+    -- Clear any previous state
+    vim.g.COLORTHEME = nil
+    package.loaded["astral"] = nil
+    astral = require("astral")
+  end)
   it("works with default", function()
     astral.setup()
     print("Test successful")

3d20e970c766616fc5543306543d79e9a60fd3b3
rootiest <chris@rootiest.com>
2024-07-27 22:08:35 -0400

    ✨ feat(usercommands): add restore option to Astral command
    Adds the option to run :Astral restore to execute the colorscheme restore function

--- lua/astral/commands.lua
+++ lua/astral/commands.lua
@@ -1,7 +1,7 @@
 local M = {}
 
-function M.astral_complete(arg_lead, _cmd_line, _cursor_pos)
-  local commands = { "reset" }
+function M.astral_complete(arg_lead)
+  local commands = { "reset", "restore" }
   return vim.tbl_filter(function(cmd)
     return vim.startswith(cmd, arg_lead)
   end, commands)
@@ -9,10 +9,13 @@ end
 
 function M.define_commands()
   local settings = require("astral.settings")
+  local astral = require("astral")
 
   vim.api.nvim_create_user_command("Astral", function(params)
     if params.args == "reset" then
       settings.reset_colorscheme()
+    elseif params.args == "restore" then
+      astral.setup(astral.config)
     else
       vim.api.nvim_err_writeln("Unknown Astral command: " .. params.args)
     end
